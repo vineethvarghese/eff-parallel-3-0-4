6dfa683c777067faf80804205f84034a49c5590e
Stephan Hoermann <srhoermann@gmail.com>
2017-02-14 11:59:32 +1100

    Adds an interpreter that keeps track of actions.

--- src/main/scala/ToyInterpreter.scala
+++ src/main/scala/ToyInterpreter.scala
@@ -23,3 +23,28 @@ object ToyInterpreter {
       }
     })
 }
+
+case class SimState(
+  actions: List[String],
+  context: Map[String, String]
+)
+
+object ToyStateInterpreter {
+  type ToyState[A] = State[SimState, A]
+  type _toyState[R] = ToyState |= R
+
+
+  def runToy[R, U, A](effects: Eff[R, A])(implicit m: Member.Aux[Toy, R, U], state: _toyState[U]): Eff[U, A] =
+    translate(effects)(new Translate[Toy, U]{
+      def apply[T](cmd: Toy[T]): Eff[U, T] = cmd match {
+        case Getting(k) => gets[U, SimState, Option[String]](m => m.context.get(k))
+        case Setting(k, v) => modify[U, SimState](m => m.copy(context = m.context + ((k, v))))
+        case Doing(v) => {
+          pure[U, Unit](Thread.sleep(5000)) >>
+          pure[U, Unit](println(v)) >>
+          modify[U, SimState](m => m.copy(actions = m.actions :+ v)) >>
+          pure("OK")
+        }
+      }
+    })
+}
